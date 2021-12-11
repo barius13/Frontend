a6e3fd7af157f96fbdf0498a0ad8040e2f84ced4
NahSahh <75503554+NahSahh@users.noreply.github.com>
2021-12-11 17:07:31 +0000

    Feat: Remade 404 page
    old one wasn't even in the center for some screen sizes

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,64 +1,60 @@
-import "focus-visible/dist/focus-visible";
-import { Link, Box, Heading, Text } from "@chakra-ui/react";
-import { Button, Flex, Center, Stack } from "@chakra-ui/react";
+import 'focus-visible/dist/focus-visible';
+import { Box, Heading, Text } from '@chakra-ui/react';
+import { Button, Flex, Center } from '@chakra-ui/react';
 
-export default function NotFoundPage() {
+export default function error() {
   return (
-      <Flex mt={"350px"} alignItems="center" justifyContent="center">
-        <Flex direction="column" width={["360px", "400px", "auto", "auto"]}>
-          <Box
-            __css={{ cursor: "default" }}
-            width={["350px", "320px", "720px", "300px", "350px", "450px"]}
-            transition="1s"
-            _hover={{
-              transform: "translateY(-4px)",
-              transition: ".5s",
-              bg: "#3B4252",
-            }}
-            px="20px"
-            py="30px"
-            rounded="10px"
+    <>
+      <Center>
+        <Flex h="90vh" alignItems="center" justifyContent="center">
+          <Flex
+            direction="column"
+            width={["360px", "400px", "auto", "auto"]}
           >
-            <Center>
-              <Flex>
-                <Heading
-                  fontSize={["40px", "40px", "40px", "40px"]}
-                  alignContent="center"
-                >
-                  Kythi - 404.
-                </Heading>
-              </Flex>
-            </Center>
-            <Center>
-              <Text
-                color="gray.400"
-                fontSize={"18px"}
-                mb={2}
-                alignContent="center"
-              >
-                {"Error, invalid page."}
-              </Text>
-            </Center>
-            <Center>
-              <Link href="/" _hover={{}}>
+            <Box boxShadow="2xl" __css={{ bg: "#3B4252", cursor: "default" }}
+              width={["320px", "320px", "320px", "320px", "320px", "450px"]}
+              transition="1s"
+              px="20px"
+              py="30px"
+              rounded="10px" >
+              <Center>
+                <Flex>
+                  <Heading
+                    fontSize={["30px", "20px", "30px", "30px"]}
+                    alignContent="center"
+                  >
+                    404
+                  </Heading>
+                </Flex>
+              </Center>
+              <Center>
+                <Flex>
+                  <Text
+                    fontSize={["18px", "18px", "18px", "18px"]}
+                    alignContent="center"
+                  >
+                    This page isn't available.
+                  </Text>
+                </Flex>
+              </Center>
+              <Center>
+                <Flex>
                 <Button
-                  transition=".6s"
-                  _hover={{
-                    transform: "translateY(-4px)",
-                    transition: ".5s",
-                    bg: "#4C566A",
-                  }}
-                  w="210px"
-                  h="33px"
-                  colorScheme="blue"
-                  variant="outline"
-                >
-                  Return Home
-                </Button>
-              </Link>
-            </Center>
-          </Box>
+              onClick={() => {location.href = "/";}}
+              mt="5px"
+              w="100%"
+              h="33px"
+              colorScheme="blue"
+              variant="outline"
+            >
+              Home.
+            </Button>
+                </Flex>
+              </Center>
+            </Box>
+          </Flex>
         </Flex>
-      </Flex>
+      </Center>
+    </>
   );
 };
