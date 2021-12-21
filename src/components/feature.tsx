4233e2d191891bbf4fa7855f3082f2199e23e936
NahSahh <75503554+NahSahh@users.noreply.github.com>
2021-12-21 18:49:00 +0000

    refactor: Stats & Feature boxes

--- src/components/feature.tsx
+++ src/components/feature.tsx
@@ -4,44 +4,42 @@ import "focus-visible/dist/focus-visible";
 import { Box, Heading, HStack } from "@chakra-ui/react";
 
 interface FeatureProps {
-    name: string;
-    value: string;
-    icon: IconType;
+  name: string;
+  value: string;
+  icon: IconType;
 }
 
 const FeatureBox: React.FC<FeatureProps> = (props) => {
   return (
     <>
-          <Box
-            __css={{ cursor: "default" }}
-            width={["350px", "320px", "720px", "300px", "350px", "450px"]}
-            transition="1s"
-            _hover={{
-              transform: "translateY(-10px)",
-              transition: ".5s",
-              bg: "#4C566A",
-            }}
-            bg={"#3B4252"}
-            px="20px"
-            py="30px"
-            shadow="xl"
-            rounded="10px"
-          >
-            <HStack>
-              <props.icon size="25" />
-              <Heading size="md" fontWeight="medium" color={"gray.400"}>
-                {props.name}
-              </Heading>
-            </HStack>
+      <Box
+        __css={{ cursor: "default" }}
+        width={["350px", "320px", "720px", "300px", "350px", "450px"]}
+        transition="1s"
+        _hover={{
+          transform: "translateY(-10px)",
+          transition: ".5s",
+          bg: "#4C566A",
+        }}
+        bg={"#3B4252"}
+        px="20px"
+        py="30px"
+        shadow="md"
+        rounded="6px"
+      >
+        <HStack>
+          <props.icon size="25" />
+          <Heading size="md" fontWeight="medium" color={"gray.400"}>
+            {props.name}
+          </Heading>
+        </HStack>
 
-            <Heading as="h4" size="sm" mt="2" fontWeight="medium">
-              {
-                props.value
-              }
-            </Heading>
-          </Box>
+        <Heading as="h4" size="sm" mt="2" fontWeight="medium">
+          {props.value}
+        </Heading>
+      </Box>
     </>
   );
-}
+};
 
 export default FeatureBox;
--- src/components/stats.tsx
+++ src/components/stats.tsx
@@ -4,45 +4,45 @@ import "focus-visible/dist/focus-visible";
 import { Spinner, Box, Heading, HStack } from "@chakra-ui/react";
 
 interface StatsProps {
-    name: string;
-    value: number;
-    icon: IconType;
+  name: string;
+  value: number;
+  icon: IconType;
 }
 
 const StatsBox: React.FC<StatsProps> = (props) => {
   return (
     <>
-          <Box
-            __css={{ cursor: "default" }}
-            transition="1s"
-            _hover={{
-              transform: "translateY(-4px)",
-              transition: ".5s",
-              bg: "#4C566A",
-            }}
-            bg={"#3B4252"}
-            width={["350px", "230px", "350px", "350px"]}
-            px="20px"
-            py="30px"
-            shadow="xl"
-            rounded="13px"
-          >
-            <HStack>
-              <props.icon size="23px" />
-              <Heading size="md" fontWeight="medium" color={"gray.400"}>
-                {props.name}
-              </Heading>
-            </HStack>
-            {props.value === undefined ? (
-              <Spinner h="20px" w="20px" mt="3" />
-            ) : (
-              <Heading as="h4" size="sm" mt="2" fontWeight="medium">
-                {props.value}
-              </Heading>
-            )}
-          </Box>
+      <Box
+        __css={{ cursor: "default" }}
+        transition="1s"
+        _hover={{
+          transform: "translateY(-4px)",
+          transition: ".5s",
+          bg: "#4C566A",
+        }}
+        bg={"#3B4252"}
+        width={["350px", "230px", "350px", "350px"]}
+        px="20px"
+        py="30px"
+        shadow="md"
+        rounded="6px"
+      >
+        <HStack>
+          <props.icon size="23px" />
+          <Heading size="md" fontWeight="medium" color={"#E5E9F0"}>
+            {props.name}
+          </Heading>
+        </HStack>
+        {props.value === undefined ? (
+          <Spinner h="20px" w="20px" mt="3" />
+        ) : (
+          <Heading as="h4" size="sm" mt="2" fontWeight="medium">
+            {props.value}
+          </Heading>
+        )}
+      </Box>
     </>
   );
-}
+};
 
 export default StatsBox;
