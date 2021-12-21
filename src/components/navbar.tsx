5d9e5aaac1dad890e6140d5ddac2f1328f3a1a31
NahSahh <75503554+NahSahh@users.noreply.github.com>
2021-12-21 18:50:24 +0000

    feat: Added Avatar to Navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,13 @@
 import * as React from "react";
 import "focus-visible/dist/focus-visible";
-import { Flex, Center, Divider, Button, Heading } from "@chakra-ui/react";
+import {
+  Flex,
+  Center,
+  Divider,
+  Button,
+  Heading,
+  Avatar,
+} from "@chakra-ui/react";
 
 interface NavBarProps {
   page: "dash" | "config" | "gallery";
@@ -18,7 +25,11 @@ const NavBarButton: React.FC<ButtonProps> = (props) => {
       ml="10px"
       size="sm"
       bg={props.isHighlighted ? "#5E81AC" : "#4C566A"}
-      _hover={props.isHighlighted ? { background: "#81A1C1" } : {background: "#3B4252"}}
+      _hover={
+        props.isHighlighted
+          ? { background: "#81A1C1" }
+          : { background: "#3B4252" }
+      }
       onClick={props.onClick}
     >
       {props.name}
@@ -48,18 +59,30 @@ const NavBar: React.FC<NavBarProps> = (props) => {
           <NavBarButton
             name="Dashboard"
             isHighlighted={props.page === "dash"}
-            onClick={() => {window.location.href = "/dashboard"}}
+            onClick={() => {
+              window.location.href = "/dashboard";
+            }}
           />
           <NavBarButton
             name="Configuration"
             isHighlighted={props.page === "config"}
-            onClick={() => { window.location.href = "/config"; }}
+            onClick={() => {
+              window.location.href = "/config";
+            }}
           />
           <NavBarButton
             name="Gallery"
             isHighlighted={props.page === "gallery"}
-            onClick={() => { window.location.href = "/gallery"; }}
+            onClick={() => {
+              window.location.href = "/gallery";
+            }}
           />
+          <Flex mr="5" position="absolute" right="0">
+            <Avatar
+              name="NahSahh"
+              src="https://cdn.discordapp.com/avatars/310088719187050507/2470c78e3a4aee23cfbe36c36bc257c5.webp?size=4096"
+            />
+          </Flex>
         </Center>
       </Flex>
     </>
