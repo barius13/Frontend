b25f73d4965e76606ca2725cf3bceee6eb201f31
Neko <78627265+Nekoyre@users.noreply.github.com>
2021-12-22 17:15:07 -0500

    feat: Boilerplate Code

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className="container mx-auto w-full text-center">
+      <h1 className='text text-slate-800 text-4xl uppercase font-bold font-mono'>In Development</h1>
+    </div>
+  )
+}
+
+export default Home
