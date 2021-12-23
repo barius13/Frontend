09f7383e1829e0018d40b91b63c21ab8eeba5d89
NahSahh <75503554+NahSahh@users.noreply.github.com>
2021-12-23 02:07:58 +0000

    refactor: reworded feature boxes & reworded

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,9 +19,7 @@ function Bios({Component, pageProps}: AppProps) {
           description: 'Kythi File host is a new service currently being made.',
         }}
       />
-      <body className='bg-polar-200'>
         <Component {...pageProps} />
-      </body>
     </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import {BsShieldLockFill} from 'react-icons/bs';
+import {BsShieldLockFill, BsHeadset} from 'react-icons/bs';
 
 export default function Index() {
   return (
@@ -7,11 +7,13 @@ export default function Index() {
       <div className='py-12 md:py-24 bg-polar-100 shadow-inner'>
         <div className='grid max-w-screen-xl px-6 mx-auto lg:px-8 xl:px-4 md:grid-cols-4 xl:grid-cols-5 gap-x-12 lg:gap-x-20'>
           <div className='self-center order-2 col-span-2 mt-12 md:order-1 md:mt-0'>
-            <h1 className=' text-3xl font-bold text-white md:text-4xl lg:text-5xl md:mb-4 lg:mb-8'>Kythi.</h1>
+            <h1 className=' text-3xl font-bold text-white md:text-4xl lg:text-5xl md:mb-4 lg:mb-2'>Kythi.</h1>
             <p className='text-lg text-gray-200 xl:text-xl lg:mb-8 xl:mb-10'>Upload files seamlessly with custom links & customisable embeds.</p>
-            <div className='flex mb-6 space-x-4'>
-              <button className='inline-block px-5 py-2 font-semibold text-white rounded-lg focus:outline-none bg-frost-400 hover:bg-frost-300'>Login</button>
-              <button className='px-5 py-2 font-semibold text-white rounded-lg focus:outline-none bg-aurora-red-200 hover:bg-aurora-red-100 inline-flex items-center'>
+            <div className='flex mb-6 space-x-4 mt-5'>
+              <button className='inline-block w-40 px-5 py-2 font-semibold text-white rounded-md bg-frost-400 hover:bg-frost-300 shadow-lg' >
+                Login
+              </button>
+              <button className='inline-blockpx-5 py-2 w-40 font-semibold text-white rounded-md bg-aurora-red-200 hover:bg-aurora-red-100 shadow-lg'>
                 Sign Up
               </button>
             </div>
@@ -25,59 +27,57 @@ export default function Index() {
             <h2 className='mb-2 text-3xl font-bold text-white md:text-4xl lg:text-5xl md:mb-4'>Features</h2>
           </div>
 
-          <div className='grid items-stretch md:grid-cols-3 content-center place-items-center gap-8'>
-            <div className='p-6 bg-polar-100 hover:bg-polar-400 lg:duration-700 ease-in-out w-full border-none rounded-md md:p-8 lg:py-12 md:shadow-lg md:hover:shadow-lg md:transition-all md:duration-500 h-56'>
+          <div className='grid items-stretch md:grid-cols-3 gap-3'>
+            <div className='p-6 bg-polar-100 hover:bg-polar-400 lg:duration-700 ease-in-out w-full border-none rounded-md md:p-8 lg:py-10 md:shadow-lg md:hover:shadow-lg md:transition-all md:duration-500 lg:h-64'>
               <div className='flex items-baseline justify-between mb-4'>
                 <h4 className='text-xl font-bold lg:text-2xl'>Personalization.</h4>
-                <svg xmlns='http://www.w3.org/2000/svg' className='h-6 w-6' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+                <svg xmlns='http://www.w3.org/2000/svg' className='h-8 w-8' fill='none' viewBox='0 0 24 24' stroke='#81A1C1'>
                   <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01' />
                 </svg>
               </div>
               <p className='mb-6 text-lg text-gray-200'>We allow you to customise your Files, Discord embeds, your Kythi profile and more!</p>
             </div>
-            <div className='p-6 bg-polar-100 border-none w-full hover:bg-polar-400 lg:duration-700 ease-in-out rounded-md md:p-8 lg:py-12 md:shadow-lg md:hover:shadow-lg md:transition-all md:duration-500 h-56'>
+            <div className='p-6 bg-polar-100 border-none w-full hover:bg-polar-400 lg:duration-700 ease-in-out rounded-md md:p-8 lg:py-10 md:shadow-lg md:hover:shadow-lg md:transition-all md:duration-500 lg:h-64'>
               <div className='flex items-baseline justify-between mb-4'>
                 <h4 className='text-xl font-bold lg:text-2xl'>Secure.</h4>
-                <BsShieldLockFill/>
+                <BsShieldLockFill className="w-6 h-6"/>
               </div>
               <p className='mb-6 text-lg text-gray-200'>All passwords are hashed using argon2i. This prevents any attackers from taking your account</p>
             </div>
-            <div className='p-6 bg-polar-100 hover:bg-polar-400 border-none rounded-md w-full md:p-8 lg:py-12 md:shadow-lg md:hover:shadow-lg md:transition-all md:duration-500 h-56'>
+            <div className='p-6 bg-polar-100 hover:bg-polar-400 border-none rounded-md w-full md:p-8 lg:py-10 md:shadow-lg md:hover:shadow-lg md:transition-all md:duration-500 lg:h-64'>
               <div className='flex items-baseline justify-between mb-4'>
-                <h4 className='text-xl font-bold lg:text-2xl'>Latest technologies</h4>
-                <svg xmlns='http://www.w3.org/2000/svg' className='h-6 w-6' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+                <h4 className='text-xl font-bold lg:text-2xl'>Various File-Types</h4>
+                <svg xmlns='http://www.w3.org/2000/svg' className='h-8 w-8' fill='none' viewBox='0 0 24 24' stroke='#EBCB8B'>
                   <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M3 15a4 4 0 004 4h9a5 5 0 10-.1-9.999 5.002 5.002 0 10-9.78 2.096A4.001 4.001 0 003 15z' />
                 </svg>
               </div>
-              <p className='mb-6 text-lg text-gray-200'>We use the latest web technologies, providing blazing fast load times, up-time and quick deployment.</p>
+              <p className='mb-6 text-lg text-gray-200'>Kythi supports many File Types, for example we allow .mp4, .mp3, .png, .jpg and more!</p>
             </div>
-            <div className='p-6 bg-polar-100 hover:bg-polar-400 lg:duration-700 border-none w-full rounded-md md:p-8 lg:py-12 md:shadow-lg md:hover:shadow-lg md:transition-all md:duration-500 h-56'>
+            <div className='p-6 bg-polar-100 hover:bg-polar-400 lg:duration-700 border-none w-full rounded-md md:p-8 lg:py-5 md:shadow-lg md:hover:shadow-lg md:transition-all md:duration-500 lg:h-64'>
               <div className='flex items-baseline justify-between mb-4'>
                 <h4 className='text-xl font-bold lg:text-2xl'>Open Source</h4>
-                <svg xmlns='http://www.w3.org/2000/svg' className='h-6 w-6' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+                <svg xmlns='http://www.w3.org/2000/svg' className='h-8 w-8' fill='none' viewBox='0 0 24 24' stroke='#BF616A'>
                   <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4' />
                 </svg>
               </div>
-              <p className='mb-6 text-lg text-gray-200'>You can view our code on <a className='text-frost-300' href='https://github.com/KythiX'>GitHub</a>. This ensures you know what happens with your data and how it happens.</p>
+              <p className='mb-6 text-lg text-gray-200'>You can view our code on <a className='text-frost-300' href='https://github.com/KythiX'>GitHub!</a> This gives you access to host this yourself as well as ensuring you know what happens with your data & how its handled.</p>
             </div>
-            <div className='p-6 bg-polar-100 hover:bg-polar-400 lg:duration-700 border-none rounded-md w-full md:p-8 lg:py-12 md:shadow-lg md:hover:shadow-lg md:transition-all md:duration-500 h-56'>
+            <div className='p-6 bg-polar-100 hover:bg-polar-400 lg:duration-700 border-none rounded-md w-full md:p-8 lg:py-10 md:shadow-lg md:hover:shadow-lg md:transition-all md:duration-500 lg:h-64'>
               <div className='flex items-baseline justify-between mb-4'>
                 <h4 className='text-xl font-bold lg:text-2xl'>Custom Domains</h4>
-                <svg xmlns='http://www.w3.org/2000/svg' className='h-6 w-6' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+                <svg xmlns='http://www.w3.org/2000/svg' className='h-8 w-8' fill='none' viewBox='0 0 24 24' stroke='#81A1C1'>
                   <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9' />
                 </svg>
               </div>
-              <p className='mb-6 text-lg text-gray-200'>You can add your own domain, free of charge.</p>
+              <p className='mb-6 text-lg text-gray-200'>You can add a variety of custom domains.</p>
             </div>
 
-            <div className='p-6 bg-polar-100 hover:bg-polar-400 lg:duration-700 border-none rounded-lg md:p-8 w-full lg:py-12 md:shadow-lg md:hover:shadow-lg md:transition-all md:duration-500 h-56'>
+            <div className='p-6 bg-polar-100 hover:bg-polar-400 lg:duration-700 border-none rounded-md md:p-8 w-full lg:py-10 md:shadow-lg md:hover:shadow-lg md:transition-all md:duration-500 lg:h-64'>
               <div className='flex items-baseline justify-between mb-4'>
                 <h4 className='text-xl font-bold lg:text-2xl'>24/7 Support</h4>
-                <svg xmlns='http://www.w3.org/2000/svg' className='h-6 w-6' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
-                  <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z' />
-                </svg>
+                  <BsHeadset className="text-2xl"/>
               </div>
-              <p className='mb-6 text-lg text-gray-200'>We provide almost fast support and answers, with a member of staff almost always being available.</p>
+              <p className='mb-6 text-lg text-gray-200'>We provide fast support and answers, with a member of staff always being available.</p>
             </div>
           </div>
         </div>
@@ -120,7 +120,7 @@ export default function Index() {
             <nav className='mt-4'>
               <ul className='space-y-2'>
                 <li>
-                  <a href='https://kythi.com' className='text-base hover:text-gray-300'>Kythi Bio's</a>
+                  <a href='https://kythi.com' className='text-base hover:text-gray-300'>Kythi Portfolio's</a>
                 </li>
                 <li>
                   <a href='https://github.com/KythiX' className='text-base hover:text-gray-300'>Our GitHub</a>
