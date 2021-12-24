a2ae4b3e13283f9b2522bcae8e0f3d9f7c99e915
NahSahh <75503554+NahSahh@users.noreply.github.com>
2021-12-24 21:57:26 +0000

    feat: Show/Hide password

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,11 +1,15 @@
+import * as React from "react";
+
 export default function Login() {
+  const loginShow = () => setShow(!show);
+  const [show, setShow] = React.useState(false);
   return (
     <div className="bg-polar-100 mx-auto sm:px-6">
       <div className="flex justify-center items-center h-screen">
         <form className="w-full max-w-md">
           <div className="rounded-lg bg-polar-200 px-8 py-8">
             <div className="divide-y-2 divide-frost-300">
-              <div className="text-white font-bold text-xl mb-2">Login.</div>
+              <div className="text-white font-bold text-xl mb-2">Login</div>
               <div />
             </div>
             <label className="relative block mt-5">
@@ -50,9 +54,47 @@ export default function Login() {
                 <input
                   className="placeholder:text-gray-400 block bg-polar-300 hover:bg-polar-400 caret-white text-white transition duration-700 delay-50 w-full h-8 rounded-md py-2 pl-10 shadow-sm focus:outline-none hover:border-frost-300 hover:ring-frost-300 focus:border-frost-400 focus:ring-frost-400 focus:ring-2 hover:ring-2 sm:text-sm"
                   placeholder="Password"
-                  type="text"
+                  type={show ? "text" : "password"}
                   name="Password"
                 />
+                <a
+                  onClick={loginShow}
+                  className="absolute inset-y-0 right-0 flex items-center pr-3"
+                >
+                  {show ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="#D8DEE9"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="#D8DEE9"
+                    >
+                      <path
+                        strokeWidth={2}
+                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                      />
+                      <path
+                        strokeWidth={2}
+                        d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                      />
+                    </svg>
+                  )}
+                </a>
               </label>
             </div>
             <a
