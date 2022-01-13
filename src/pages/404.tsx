4a4ea98ea7d5f7f0df6f8f52d77c20d6d3913cff
Jake <75503554+NahSahh@users.noreply.github.com>
2022-01-13 12:38:41 +0000

    fix: 404 home message different for users logged in

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,22 +1,25 @@
 import Link from "next/link";
+import { useUser } from "../components/user";
 import Footer from "../components/footer";
 
 export default function Page404() {
+  const { user } = useUser();
+
   return (
     <>
       <div className="bg-polar-200">
         <div className="flex justify-center items-center h-screen grid-rows-1	text-white">
           <div className="text-center">
-            <h1 className="text-5xl font-bold text-snow-200">
+            <h1 className="lg:text-5xl md:text-5xl text-4xl font-bold text-snow-200">
               404 • Unavailable
             </h1>
-            <p className="text-xl text-snow-100">
-              The page you are looking for does not exist.
+            <p className="text-xl text-snow-100 px-4">
+              The page you are looking for is unavailble or does not exist.
             </p>
 
-            <Link href="/" passHref>
-              <button className="py-2 w-40 font-semibold text-white rounded-md bg-aurora-red-200 hover:bg-aurora-red-100 shadow-lg mt-3">
-                Home
+            <Link href={user ? "/dashboard" : "/"} passHref>
+              <button className="btn normal-case border-0 btn-sm w-36 h-10 bg-frost-400 hover:bg-frost-300 shadow-lg mt-3">
+                {user ? "Dashboard" : "Home"}
               </button>
             </Link>
           </div>
