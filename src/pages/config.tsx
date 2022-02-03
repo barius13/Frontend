49e23bcaad2ecc8d6417ababfebc3b16b9f9292f
Jake <75503554+NahSahh@users.noreply.github.com>
2022-02-03 18:30:39 +0000

    refactor: redesigned embed modal, fix: embed color hidden not invisible,

--- src/pages/config.tsx
+++ src/pages/config.tsx
@@ -49,9 +49,9 @@ export default function Config() {
             <div />
           </div>
           <div className="mt-2 text-snow-100">
-            Configs are for screenshot uploaders such as the ones listed below,
-            it allows a user to quickly take images & videos aswell as upload
-            them through the use of a keybind.
+            Configs are created for screenshot uploaders such as the ones listed
+            below, it allows a user to quickly take images & videos aswell as
+            upload them through the use of a keybind.
           </div>
           <button className="btn w-full border-0 btn-sm h-10 mt-6 normal-case bg-frost-400 hover:bg-frost-300 duration-300 ">
             Sharex (Windows)
@@ -100,22 +100,6 @@ export default function Config() {
             >
               Edit Config
             </label>
-            <label
-              onClick={() => {
-                sendToast("Successfully deleted preset!", "success");
-              }}
-              className="bg-aurora-red-300 hover:bg-aurora-red-400 duration-300 normal-case btn lg:w-36 md:w-36 w-full mt-2 border-0"
-            >
-              Delete Preset
-            </label>
-            <label
-              onClick={() => {
-                sendToast("Successfully created preset!", "success");
-              }}
-              className="bg-[#239d56] hover:bg-[#1f8b4d] duration-300 btn normal-case lg:w-36 md:w-36 w-full mt-2 border-0"
-            >
-              Create Preset
-            </label>
           </div>
         </div>
       </div>
@@ -123,157 +107,182 @@ export default function Config() {
       <input type="checkbox" id="Embed-Editor" className="modal-toggle" />
       <div className="modal p-2 duration-200">
         <div className="modal-box rounded-md bg-polar-100 p-6">
-          <h1 className="font-semibold text-2xl mb-4">Embed Editor</h1>
-          <div className="divide-y-2 divide-white">
-            <div />
-            <div />
-          </div>
           <div>
-            <div className="flex flex-col mb-4 mt-2">
-              <span className="text-snow-100">
-                There is many placeholders, to view them type % for options
-              </span>
-
+            <div className="flex flex-row justify-between">
+              <h1 className="font-semibold text-2xl mb-4">
+                Discord Embed Editor
+              </h1>
               <label
-                className={`cursor-pointer label mr-auto ${
-                  currentEmbedData.color === "RANDOM" && "invisible"
-                }`}
+                htmlFor="Embed-Editor"
+                className="bg-polar-200 border-0 normal-case hover:bg-polar-400 btn btn-sm rounded-md cursor-pointer text-center text-white font-medium mt-1 transition duration-500"
               >
-                <span>Embed Color</span>
-                <input
-                  name="color"
-                  type="color"
-                  value={currentEmbedData.color ?? "#FFFFFF"}
-                  onChange={updateEmbedData}
-                  className="bg-polar-100 mr-auto ml-2"
-                />
+                X
               </label>
-
-              <label className="cursor-pointer label mr-auto">
-                <span>Random Embed Color</span>
-                <input
-                  name="color"
-                  type="checkbox"
-                  className="toggle toggle-md toggle-accent ml-4"
-                  checked={currentEmbedData.color === "RANDOM"}
-                  onChange={updateEmbedData}
-                />
-              </label>
-
-              <input
-                name="siteText"
-                placeholder="SiteName"
-                className="bg-polar-300 mt-3 w-full rounded-md p-2 hover:bg-polar-400 focus:outline-none transition duration-500 delay-75 focus:duration-500 focus:bg-polar-400"
-                value={currentEmbedData.siteText ?? ""}
-                onChange={updateEmbedData}
-              />
-              <label className="input-group mt-3">
-                <span className="bg-gray-700 border-r-2 border-gray-300">
-                  https://
-                </span>
-                <input
-                  name="siteUrl"
-                  placeholder="SiteName Url"
-                  className="bg-polar-300 w-full rounded-md p-2 hover:bg-polar-400 focus:outline-none transition duration-500 delay-75 focus:duration-500 focus:bg-polar-400"
-                  value={currentEmbedData.siteUrl ?? ""}
-                  onChange={updateEmbedData}
-                />
-              </label>
-
-              <input
-                name="authorText"
-                placeholder="Author"
-                className="bg-polar-300 mt-3 w-full rounded-md p-2 hover:bg-polar-400 focus:outline-none transition duration-500 delay-75 focus:duration-500 focus:bg-polar-400"
-                value={currentEmbedData.authorText ?? ""}
-                onChange={updateEmbedData}
-              />
-
-              <label className="input-group mt-3">
-                <span className="bg-gray-700 border-r-2 border-gray-300">
-                  https://
-                </span>
-                <input
-                  name="authorUrl"
-                  placeholder="Author Url"
-                  className="bg-polar-300 w-full rounded-md p-2 hover:bg-polar-400 focus:outline-none transition duration-500 delay-75 focus:duration-500 focus:bg-polar-400"
-                  value={currentEmbedData.authorUrl ?? ""}
-                  onChange={updateEmbedData}
-                />
-              </label>
-
-              <input
-                name="title"
-                placeholder="Title"
-                className="bg-polar-300 w-full mt-3 rounded-md p-2 hover:bg-polar-400 focus:outline-none transition duration-500 delay-75 focus:duration-500 focus:bg-polar-400"
-                value={currentEmbedData.title ?? ""}
-                onChange={updateEmbedData}
-              />
-              <input
-                name="description"
-                placeholder="Description"
-                className="bg-polar-300 mt-3 h-10 rounded-md p-2 hover:bg-polar-400 focus:outline-none transition duration-500 delay-75 focus:duration-500 focus:bg-polar-400"
-                value={currentEmbedData.description ?? ""}
-                onChange={updateEmbedData}
-              />
             </div>
-            <label className="cursor-pointer label mr-auto">
-              <span>Toggle Embed</span>
-              <input
-                name="enabled"
-                type="checkbox"
-                className="toggle toggle-md toggle-accent ml-4"
-                checked={currentEmbedData.enabled}
-                onChange={updateEmbedData}
-              />
-            </label>
-            <div className="btn-group flex justify-center mb-2">
-              <button className="btn normal-case">-</button>
-              {user.embeds.map((embed, index) => (
-                <button
-                  key={index}
-                  className={`btn ${
-                    currentPage === index && "bg-frost-400 hover:bg-frost-300"
-                  }`}
+            <div className="divide-y-2 divide-white">
+              <div />
+              <div />
+            </div>
+            <div>
+              <div className="flex flex-col mb-4 mt-2">
+                <span className="text-snow-100">
+                  There is many placeholders, to view them type % for options
+                </span>
+
+                <label className="input-group mt-3">
+                  <span className="bg-gray-700 border-r-2 border-gray-300">
+                    https://
+                  </span>
+                  <input
+                    name="siteUrl"
+                    placeholder="SiteName Url"
+                    className="bg-polar-300 w-full rounded-md p-2 hover:bg-polar-400 focus:outline-none transition duration-500 delay-75 focus:duration-500 focus:bg-polar-400"
+                    value={currentEmbedData.siteUrl ?? ""}
+                    onChange={updateEmbedData}
+                  />
+                </label>
+
+                <input
+                  name="siteText"
+                  placeholder="SiteName"
+                  className="bg-polar-300 mt-3 w-full rounded-md p-2 hover:bg-polar-400 focus:outline-none transition duration-500 delay-75 focus:duration-500 focus:bg-polar-400"
+                  value={currentEmbedData.siteText ?? ""}
+                  onChange={updateEmbedData}
+                />
+
+                <label className="input-group mt-3">
+                  <span className="bg-gray-700 border-r-2 border-gray-300">
+                    https://
+                  </span>
+                  <input
+                    name="authorUrl"
+                    placeholder="Author Url"
+                    className="bg-polar-300 w-full rounded-md p-2 hover:bg-polar-400 focus:outline-none transition duration-500 delay-75 focus:duration-500 focus:bg-polar-400"
+                    value={currentEmbedData.authorUrl ?? ""}
+                    onChange={updateEmbedData}
+                  />
+                </label>
+
+                <input
+                  name="authorText"
+                  placeholder="Author"
+                  className="bg-polar-300 mt-3 w-full rounded-md p-2 hover:bg-polar-400 focus:outline-none transition duration-500 delay-75 focus:duration-500 focus:bg-polar-400"
+                  value={currentEmbedData.authorText ?? ""}
+                  onChange={updateEmbedData}
+                />
+
+                <input
+                  name="title"
+                  placeholder="Title"
+                  className="bg-polar-300 w-full mt-3 rounded-md p-2 hover:bg-polar-400 focus:outline-none transition duration-500 delay-75 focus:duration-500 focus:bg-polar-400"
+                  value={currentEmbedData.title ?? ""}
+                  onChange={updateEmbedData}
+                />
+                <input
+                  name="description"
+                  placeholder="Description"
+                  className="bg-polar-300 mt-3 mb-2 h-10 rounded-md p-2 hover:bg-polar-400 focus:outline-none transition duration-500 delay-75 focus:duration-500 focus:bg-polar-400"
+                  value={currentEmbedData.description ?? ""}
+                  onChange={updateEmbedData}
+                />
+              </div>
+            </div>
+            <div className="flex flex-col">
+              <label
+                onClick={() => {
+                  API.updateEmbedSettings(currentEmbedData.id, {
+                    enabled: currentEmbedData.enabled,
+                    color: currentEmbedData.color as string,
+                    siteText: currentEmbedData.siteText,
+                    siteUrl: currentEmbedData.siteUrl,
+                    authorText: currentEmbedData.authorText,
+                    authorUrl: currentEmbedData.authorUrl,
+                    title: currentEmbedData.title,
+                    description: currentEmbedData.description,
+                  })
+                    .then((data) => {
+                      sendToast(data.message, "success");
+                    })
+                    .catch((err) => {
+                      sendToast(err.data.message, "error");
+                    });
+                }}
+                className="bg-[#239d56] btn hover:bg-[#1f8b4d] capitalize cursor-pointer text-center text-white font-medium border-0"
+              >
+                Save Changes
+              </label>
+              <label
+                onClick={() => {
+                  sendToast("Enabled Embed", "success");
+                }}
+                className="w-full bg-aurora-red-300 hover:bg-aurora-red-400 duration-300 normal-case btn border-0 mt-3"
+              >
+                Enable Embed
+              </label>
+              <div className="mt-3 flex justify-center space-x-4">
+                <label
                   onClick={() => {
-                    setCurrentPage(index);
-                    setCurrentEmbedData(embed);
+                    sendToast("Successfully deleted preset!", "success");
                   }}
+                  className="bg-aurora-red-300 hover:bg-aurora-red-400 duration-300 normal-case btn border-0 w-56"
                 >
-                  {index + 1}
-                </button>
-              ))}
-              <button className="btn normal-case"> - </button>
+                  Delete Preset
+                </label>
+                <label
+                  onClick={() => {
+                    sendToast("Successfully created preset!", "success");
+                  }}
+                  className="bg-[#239d56] hover:bg-[#1f8b4d] duration-300 btn normal-case border-0 w-56"
+                >
+                  Create Preset
+                </label>
+              </div>
+              <div className="btn-group flex justify-center mt-4">
+                <button className="btn normal-case">{"<"}</button>
+                {user.embeds.map((embed, index) => (
+                  <button
+                    key={index}
+                    className={`btn ${
+                      currentPage === index && "bg-frost-400 hover:bg-frost-300"
+                    }`}
+                    onClick={() => {
+                      setCurrentPage(index);
+                      setCurrentEmbedData(embed);
+                    }}
+                  >
+                    {index + 1}
+                  </button>
+                ))}
+                <button className="btn normal-case"> {">"} </button>
+              </div>
+              <div>
+                {" "}
+                <label className="cursor-pointer label">
+                  <span>Random Color</span>
+                  <input
+                    name="color"
+                    type="checkbox"
+                    className="toggle toggle-md toggle-accent ml-4"
+                    checked={currentEmbedData.color === "RANDOM"}
+                    onChange={updateEmbedData}
+                  />
+                </label>
+                <label
+                  className={`cursor-pointer label ${
+                    currentEmbedData.color === "RANDOM" && "hidden"
+                  }`}
+                >
+                  <span>Embed Color</span>
+                  <input
+                    name="color"
+                    type="color"
+                    value={currentEmbedData.color ?? "#FFFFFF"}
+                    onChange={updateEmbedData}
+                    className="bg-polar-100 ml-auto"
+                  />
+                </label>
+              </div>
             </div>
-          </div>
-          <div className="space-x-2">
-            <label
-              onClick={() => {
-                API.updateEmbedSettings(currentEmbedData.id, {
-                  enabled: currentEmbedData.enabled,
-                  color: currentEmbedData.color as string,
-                  siteText: currentEmbedData.siteText,
-                  siteUrl: currentEmbedData.siteUrl,
-                  authorText: currentEmbedData.authorText,
-                  authorUrl: currentEmbedData.authorUrl,
-                  title: currentEmbedData.title,
-                  description: currentEmbedData.description,
-                }).then((data) => {
-                  sendToast(data.message, "success");
-                })
-                .catch((err) => {
-                  sendToast(err.data.message, "error");
-                });
-              }}
-              className="bg-[#239d56] btn hover:bg-[#1f8b4d] capitalize cursor-pointer text-center text-white font-medium border-0"
-            >
-              Save Embed
-            </label>
-            <label
-              htmlFor="Embed-Editor"
-              className="bg-polar-300 btn hover:bg-polar-200 capitalize cursor-pointer text-center text-white font-medium mt-2 border-0"
-            >
-              Cancel
-            </label>
           </div>
         </div>
       </div>
