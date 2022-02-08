9210107b0b1b20b2b7ad4ae47ae1a1b6f4520bf3
Kurp <78627265+Kurpp@users.noreply.github.com>
2022-02-08 06:59:37 -0500

    fix: Apply css to svgs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     content: [
+        "./public/svgs/**/*.{js,ts,jsx,tsx}",
         "./src/pages/**/*.{js,ts,jsx,tsx}",
         "./src/components/**/*.{js,ts,jsx,tsx}",
         "./src/utils/**/*.{js,ts,jsx,tsx}",
