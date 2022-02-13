86a1b2106864d9b8e7b132cda52102f1d11b7662
Jake <75503554+NahSahh@users.noreply.github.com>
2022-02-13 04:54:07 +0000

    feat: working input group

--- src/components/inputgroup.tsx
+++ src/components/inputgroup.tsx
@@ -1,49 +1,46 @@
 import { FC } from "react";
 
 interface Input {
-  text: string;
+  text?: string;
+  show?: boolean;
+  value?: any;
+  onChange?: any;
+  textCname?: string;
+  inputCname?: string;
   PlaceHolder: string;
   keepBorder?: boolean;
-  textOptions?: {
-    rounded?: boolean;
-    roundedLeft?: boolean;
-    roundedRight?: boolean;
-  };
-  inputOptions?: {
-    rounded?: boolean;
-    roundedLeft?: boolean;
-    roundedRight?: boolean;
-  };
 }
 
 const Input: FC<Input> = ({
   text,
-  PlaceHolder,
+  show,
+  value,
+  onChange,
+  textCname = "",
+  inputCname = "",
   keepBorder,
-  textOptions,
-  inputOptions,
+  PlaceHolder,
 }) => {
   return (
     <>
-      <span
-        className={`flex items-center px-2 p-2 bg-zinc-700 text-white text-sm max-w-[82px] ${
-          textOptions?.rounded && "rounded"
-        } ${textOptions?.roundedLeft && "rounded-l"} ${
-          textOptions?.roundedRight && "rounded-r"
-        }`}
-      >
-        {text}
-      </span>
-      <input
-        className={`px-2 bg-polar-300 border-r-polar-400 ${
-          keepBorder && "border-r-2"
-        } focus:outline-none w-32 transition duration-500 p-2 delay-75 focus:duration-500 focus:bg-polar-400 ${
-          inputOptions?.rounded && "rounded"
-        } ${inputOptions?.roundedLeft && "rounded-l"} ${
-          inputOptions?.roundedRight && "rounded-r"
-        }`}
-        placeholder={PlaceHolder}
-      />
+      <div className="flex">
+        <span
+          className={`flex items-center px-2 p-2 bg-zinc-700 text-white text-sm ${
+            show || "hidden"
+          } ${textCname}`}
+        >
+          {text}
+        </span>
+        <input
+          value={value}
+          onChange={onChange}
+          spellCheck={false}
+          className={`px-2 bg-polar-300 border-r-polar-400 ${
+            keepBorder && "border-r-2"
+          } focus:outline-none transition duration-500 p-2 delay-75 focus:duration-500 focus:bg-polar-400 ${inputCname}`}
+          placeholder={PlaceHolder}
+        />
+      </div>
     </>
   );
 };
